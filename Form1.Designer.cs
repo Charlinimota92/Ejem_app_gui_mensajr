6caa24aa1ff0e0bd09d48b8fadd3b56f29da4f65
heurilinares <heurilinares@DESKTOP-7F9G3G4>
2024-02-11 19:06:14 -1200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Ejem_app_gui_mensajr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.btn_Mostrar_Mensaje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salir.Location = new System.Drawing.Point(145, 171);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(148, 51);
+            this.btn_Salir.TabIndex = 0;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // btn_Mostrar_Mensaje
+            // 
+            this.btn_Mostrar_Mensaje.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Mostrar_Mensaje.Location = new System.Drawing.Point(145, 84);
+            this.btn_Mostrar_Mensaje.Name = "btn_Mostrar_Mensaje";
+            this.btn_Mostrar_Mensaje.Size = new System.Drawing.Size(148, 52);
+            this.btn_Mostrar_Mensaje.TabIndex = 1;
+            this.btn_Mostrar_Mensaje.Text = "Mostrar Mensaje";
+            this.btn_Mostrar_Mensaje.UseVisualStyleBackColor = true;
+            this.btn_Mostrar_Mensaje.Click += new System.EventHandler(this.btn_Mostrar_Mensaje_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 313);
+            this.Controls.Add(this.btn_Mostrar_Mensaje);
+            this.Controls.Add(this.btn_Salir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.Button btn_Mostrar_Mensaje;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejem_app_gui_mensajr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Mostrar_Mensaje_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desarrollo de aplicaciones");
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+            resultado = MessageBox.Show("Desea salir de la aplicación?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+          
+
+            if (resultado==DialogResult.Yes)
+            {
+                Close();
+            }
+            
+         
+        }
+    }
+}
